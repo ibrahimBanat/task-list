2c701195c408abbce36364345177c75d2ac4ba1d
Ibrahim Banat <i.banat@syarah.com>
2026-02-17 12:47:12 +0300

    blade templiting and directives

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<h1>
+   The list of tasks
+</h1>
+
+<div>
+{{--    @if(count($tasks) > 0)--}}
+{{--        @foreach($tasks as $task)--}}
+{{--            <div>--}}
+{{--                {{$task->title}}--}}
+{{--            </div>--}}
+{{--        @endforeach--}}
+{{--    @else--}}
+{{--        <div>There are no tasks!</div>--}}
+{{--    @endif--}}
+    @forelse($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                {{$task->title}}
+            </a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+</div>
