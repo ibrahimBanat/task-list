553f3518af8ad6bbe2b1976c02803508e8ba96b0
Ibrahim Banat <i.banat@syarah.com>
2026-02-17 12:20:13 +0300

    routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/hello", function () {
+    return 'Hello';
+})->name('hello');
+
+Route::get('hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return "Hello {$name}!";
+});
