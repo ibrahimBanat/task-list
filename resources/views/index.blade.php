3636eb29ecd8cfc9dfd695fec41325871d0f7a1b
Ibrahim Banat <i.banat@syarah.com>
2026-02-17 14:44:46 +0300

    add blade layouts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,17 @@
-<h1>
-   The list of tasks
-</h1>
+@extends('layouts.app')
 
-<div>
-{{--    @if(count($tasks) > 0)--}}
-{{--        @foreach($tasks as $task)--}}
-{{--            <div>--}}
-{{--                {{$task->title}}--}}
-{{--            </div>--}}
-{{--        @endforeach--}}
-{{--    @else--}}
-{{--        <div>There are no tasks!</div>--}}
-{{--    @endif--}}
-    @forelse($tasks as $task)
-        <div>
-            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
-                {{$task->title}}
-            </a>
-        </div>
-    @empty
-        <div>There are no tasks!</div>
-    @endforelse
-</div>
+@section('title', 'The list of tasks')
+
+@section('content')
+    <div>
+        @forelse($tasks as $task)
+            <div>
+                <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                    {{$task->title}}
+                </a>
+            </div>
+        @empty
+            <div>There are no tasks!</div>
+        @endforelse
+    </div>
+@endsection
